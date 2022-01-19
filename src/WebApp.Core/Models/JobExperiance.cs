157bff513af633226a6d63d4a717c186ab19e7b6
Tom <tomianwright@gmail.com>
2022-01-19 22:24:02 +0000

    Cv Prototyping

---
+++ src/WebApp.Core/Models/JobExperiance.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Core.Models;
+
+public class JobExperience
+{
+    public int Id { get; set; }
+    public string CompanyName { get; set; }
+    public string Position { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string StartDateString => StartDate.ToString("d");
+    public string EndDateString => EndDate?.ToString("d") ?? "Present";
+}
