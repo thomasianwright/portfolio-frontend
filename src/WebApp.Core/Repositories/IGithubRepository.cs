02b1dbe21539280e6a9048b42c552af8dec2d6d9
Tom <tomianwright@gmail.com>
2022-01-19 00:07:38 +0000

    INIT

---
+++ src/WebApp.Core/Repositories/IGithubRepository.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using WebApp.Core.Models;
+
+namespace WebApp.Core.Repositories;
+
+public interface IGithubRepository
+{
+    [Headers("Content-Type: application/json")]
+    [Get("/users/thomasianwright/repos")]
+    public Task<IEnumerable<Repository>> GetRepositories();
+}
